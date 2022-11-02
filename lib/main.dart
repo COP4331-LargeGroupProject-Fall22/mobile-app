9ddaa2f1916a9655f3e674d2b35cf334e6ba3568
ChillXoro <chillxoro@gmail.com>
2022-11-02 18:25:30 -0400

    added stephens code into branch to add to main because it was added as a seperate origin and i cant merge so im making a branch with his code from inital

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:smart_chef/routes/routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      routes: Routes.getroutes,
+    );
+  }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:smart_chef/screens/StartupScreen.dart';
+import 'package:smart_chef/screens/RecipeScreen.dart';
+import 'package:smart_chef/screens/IngredientScreen.dart';
+import 'package:smart_chef/screens/ShoppingCartScreen.dart';
+import 'package:smart_chef/screens/UserProfileScreen.dart';
+
+class Routes {
+  static const String startupScreen = '/startup';
+  static const String recipeScreen = '/recipe';
+  static const String ingredientScreen = '/food';
+  static const String shoppingCartScreen = '/cart';
+  static const String userProfileScreen = '/user';
+
+  // routes of pages in the app
+  static Map<String, Widget Function(BuildContext)> get getroutes => {
+    '/': (context) => StartupScreen(),
+    startupScreen: (context) => StartupScreen(),
+    recipeScreen: (context) => RecipeScreen(),
+    ingredientScreen: (context) => IngredientsScreen(),
+    shoppingCartScreen: (context) => ShoppingCartScreen(),
+    userProfileScreen: (context) => UserProfileScreen(),
+  };
+}
