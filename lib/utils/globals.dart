787c72ce210064fac643163b6a92338b40005d22
Shrewstar <stephenmaliszewski15@gmail.com>
2022-11-10 15:22:08 -0500

    Attached api mostly to sign in

---
+++ lib/utils/globals.dart
@@ -0,0 +1,10 @@
+
+
+RegExp emailValidation = RegExp(
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+
+bool isEmail(email) {
+  return emailValidation.hasMatch(email);
+}
+
+const String API_PREFIX = "https://api-smart-chef.herokuapp.com/";
